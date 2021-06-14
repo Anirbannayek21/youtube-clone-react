5a0e3081f0e12a46e303b29958285a7b036d0254
Anirbannayek21 <anirbannayek2@gmail.com>
2021-06-14 18:53:14 +0530

    fixed env ver

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-console.log(process.env.REACT_APP_YT_API_KEY);
+// console.log(process.env.REACT_APP_YT_API_KEY);
 const axiosInstance = axios.create({
     baseURL: "https://youtube.googleapis.com/youtube/v3",
     params: {
-        key: process.env.REACT_APP_YT_API_KEY
+        key: "AIzaSyCkvCNEvBsblhfWRs1O_vcAU7leU3ybhts"
     }
 })
 
